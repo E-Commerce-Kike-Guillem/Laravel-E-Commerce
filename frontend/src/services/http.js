89a92dd825d190fd02a3a8df1f66b56db51c67aa
Kike Navarro <kikenavarrojover@gmail.com>
2026-06-01 23:51:29 +0200

    fix urls

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // Envía las cookies de sesión
   withXSRFToken: true,   // <-- ¡LA PIEZA QUE FALTABA! Extrae el token CSRF automáticamente
   headers: {
